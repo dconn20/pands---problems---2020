3b9b8367ed6a7ac8c24a1a60ee2d7c1ba866556f
dconn20 <damien.connolly85@gmail.com>
2020-02-19 18:17:49 +0000

    added

--- weekday.py
+++ weekday.py
@@ -7,13 +7,13 @@
 import datetime
 import calendar
 
-L={0:"Monday", 1:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday", 7:"Sunday"}
+dayname ={0:"Monday", 1:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday", 7:"Sunday"}
 
 now = datetime.datetime.now()
 day = now.weekday
+weekend = (5, 7)
 
-
-if day == 5
+if day == weekend:
     print ("Today is a week day")
-    else:
-        print ("Weekend")
+else:
+    print ("Weekend")
