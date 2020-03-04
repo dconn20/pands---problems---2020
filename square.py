85317a512c1d28fda49e272ee1b1834a6c5e8882
dconn20 <damien.connolly85@gmail.com>
2020-03-04 20:17:26 +0000

    added

--- square.py
+++ square.py
@@ -1,17 +1,26 @@
 # Damien Connolly
 # A funtion to square numbers
 
-n = float(input("enter a positive number "))
 
+#n = float(input("enter a positive number "))
 
 def sqrt(n): 
-    #n ** (1/2)
+    
     ans = n ** (1/2)
     print ("The square root of the number is", ans,)
     return ans
-    #print ("The square root of the number is", ans,)
+    
 
-sqrt(n)
+#sqrt(n)
 
 
+def newsq(number, number_iters = 500):
+    x = float(number)
+    for i in range(number_iters):
+        number = 0.5 * (number + x / number)
+        print(number)
+        return number
+    #print(number)
 
+
+newsq(9)
